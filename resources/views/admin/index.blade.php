d56ec7b4de3407b4df92784bd9f7a4a46ed00e9d
AdiatsaPutra <santikosetyo77@gmail.com>
2021-07-03 20:40:04 +0700

    Added Total

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -5,38 +5,32 @@
 @section('content')
 <div class="row">
   <div class="col-12 col-sm-4 col-lg-4">
-    <div class="card">
+    <div class="card text-center">
       <div class="card-header">
-        <h4>Customer</h4>
+        <h4>Total Customer</h4>
       </div>
-      <div class="collapse show" id="mycard-collapse">
-        <div class="card-body">
-          You can show or hide this card.
-        </div>
+      <div class="my-3">
+       <h1>{{ App\Models\Customer::all()->count() }}</h1>
       </div>
     </div>
   </div>
   <div class="col-12 col-sm-4 col-lg-4">
-    <div class="card">
+    <div class="card text-center">
       <div class="card-header">
         <h4>Total Barang Import</h4>
       </div>
-      <div class="collapse show" id="mycard-collapse">
-        <div class="card-body">
-          You can show or hide this card.
-        </div>
+      <div class="my-3">
+       <h1>{{ App\Models\BarangImport::all()->count() }}</h1>
       </div>
     </div>
   </div>
   <div class="col-12 col-sm-4 col-lg-4">
-    <div class="card">
+    <div class="card text-center">
       <div class="card-header">
         <h4>Total Barang Export</h4>
       </div>
-      <div class="collapse show" id="mycard-collapse">
-        <div class="card-body">
-          You can show or hide this card.
-        </div>
+      <div class="my-3">
+       <h1>{{ App\Models\BarangExport::all()->count() }}</h1>
       </div>
     </div>
   </div>
