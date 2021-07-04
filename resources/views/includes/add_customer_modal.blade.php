06fd451dca9550e55d674787ea465b92153f5c45
AdiatsaPutra <santikosetyo77@gmail.com>
2021-07-04 17:37:25 +0700

    Add store cusromer

--- resources/views/includes/add_customer_modal.blade.php
+++ resources/views/includes/add_customer_modal.blade.php
@@ -2,19 +2,20 @@
     <div class="modal-dialog">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Tambah Customer</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <form>
+        <form action="{{ route('customers.store') }}" method="post">
+        @csrf
         <div class="modal-body">
                 <div class="mb-3">
                     <label for="nama" class="form-label">Nama Customer</label>
-                    <input type="text" class="form-control" id="nama" placeholder="Nama Customer">
+                    <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Customer">
                 </div>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
             </div>
         </form>
     </div>
